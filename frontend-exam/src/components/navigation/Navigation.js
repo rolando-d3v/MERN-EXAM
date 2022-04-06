a7f5e1e82fb329674fbbfb5f13d495cb0d490dc3
Rolando Mamani Flores <rmamani@SEVERILEX.COM.PE>
2022-04-06 15:09:34 -0500

    red

--- frontend-exam/src/components/navigation/Navigation.js
+++ frontend-exam/src/components/navigation/Navigation.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <Navbar style={{background: "#20232a"}}  variant="dark" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Examen - X</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
