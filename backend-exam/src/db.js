c74306e1c0059630e9caddcaf317fba7e185a2ef
unknown <landon3388@gmail.com>
2022-07-17 20:19:43 -0500

    ed

--- backend-exam/src/db.js
+++ backend-exam/src/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const db = process.env.DB_PRODUCTION
+const db = process.env.DB
+// const db = process.env.DB_PRODUCTION
 mongoose.connect(db, {
     useCreateIndex: true,
     useUnifiedTopology:true,
